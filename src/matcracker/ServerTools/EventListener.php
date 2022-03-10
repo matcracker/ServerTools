fb3c8c2a2606b9f55b4cf7580e9b6867111566b6
matcracker <matcracker@users.noreply.github.com>
2022-03-10 21:27:45 +0100

    Minor code improvement

--- src/matcracker/ServerTools/EventListener.php
+++ src/matcracker/ServerTools/EventListener.php
@@ -25,21 +25,16 @@ namespace matcracker\ServerTools;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerEditBookEvent;
-use pocketmine\item\ItemFactory;
-use pocketmine\item\WrittenBook;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\TextFormat;
 use function basename;
-use function count;
 use function file_put_contents;
 use function is_writable;
 
 final class EventListener implements Listener{
 
-	private Main $plugin;
+	public function __construct(private Main $plugin){
 
-	public function __construct(Main $plugin){
-		$this->plugin = $plugin;
 	}
 
 	public function onPlayerEditBook(PlayerEditBookEvent $event) : void{
@@ -64,26 +59,22 @@ final class EventListener implements Listener{
 		}
 
 		$newFileContent = "";
-		for($pageId = 0; $pageId < count($oldBook->getPages()); $pageId++){
-			$newFileContent .= $oldBook->getPageText($pageId) ?? "";
+		foreach($oldBook->getPages() as $page){
+			$newFileContent .= $page->getText();
 		}
 
 		if(file_put_contents($filePath, $newFileContent) === false){
 			$player->sendMessage(Main::formatMessage(TextFormat::RED . "Error while saving the new content of file \"$fileName\"."));
 		}else{
-			$bookSlot = $player->getInventory()->getHeldItemIndex();
+			$newBook = $event->getNewBook();
 			//Remove the book from the hotbar
 			$this->plugin->getScheduler()->scheduleDelayedTask(
 				new ClosureTask(
-					static function() use ($player, $bookSlot) : void{
-						if($player !== null){
-							$book = $player->getInventory()->getHotbarSlotItem($bookSlot);
-							if($book instanceof WrittenBook){
-								$player->getInventory()->setItem($bookSlot, ItemFactory::air());
-							}
-						}
+					static function() use ($player, $newBook) : void{
+						$player?->getInventory()->remove($newBook);
 					}
-				), 1
+				),
+				1
 			);
 
 			$player->sendMessage(Main::formatMessage(TextFormat::GREEN . "File successfully edited!"));
